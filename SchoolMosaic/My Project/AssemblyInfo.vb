117fc293479a5ae00ca9f19d50a96751a6c6ffb3
Le Stagiaire <f.goussot@outlook.com>
2023-08-28 16:49:22 +0200

    Ajoutez des fichiers projet.

---
+++ SchoolMosaic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("SchoolMosaic Server")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Le Stagiaire")>
+<Assembly: AssemblyProduct("SchoolMosaic")>
+<Assembly: AssemblyCopyright("Licence Open Souce")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("f55b6158-b107-406c-b6f0-0be2d7524d79")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
